ac4f5b7bff9930d6ed8583b6b6eed217a8e65ed1
PabloGM1204 <115071457+PabloGM1204@users.noreply.github.com>
2022-12-10 16:43:47 +0100

    Add files via upload

--- js/top.js
+++ js/top.js
@@ -1,22 +1,33 @@
-// Get the button
-let mybutton = document.getElementById("myBtn");
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function() {scrollFunction()};
-    
-    function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            mybutton.style.display = "block";
-        } else {
-            mybutton.style.display = "none";
-        }
-    }
+let slideIndex = 1;
+showSlides(slideIndex);
 
-// When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+// Boton de ver mas 
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -34,4 +45,14 @@ acc[i].addEventListener("click", function() {
         panel.style.display = "block";
     }
 });
+}
+
+// Para el menu hamburguesa
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
